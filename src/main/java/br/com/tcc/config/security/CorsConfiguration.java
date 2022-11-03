f9ba37147348f435bd6cb6db1dd1106fc4b0207a
Rwan Souza <rwan.souza@hotmail.com>
2022-11-03 12:50:56 -0300

    fix: liberando cors para todas as requisições

--- src/main/java/br/com/tcc/config/security/CorsConfiguration.java
+++ src/main/java/br/com/tcc/config/security/CorsConfiguration.java
@@ -8,14 +8,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfiguration implements WebMvcConfigurer {
 
     @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-            .allowedOrigins("*")
-            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
-        
-        
+    public void addCorsMappings(CorsRegistry registry) {        
         registry.addMapping("/**")
         .allowedOrigins("http://localhost:4200")
-       .allowedMethods("GET", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
     }
 }
